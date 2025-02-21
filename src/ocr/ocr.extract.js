afab6d72c0d9811f83e641eda861a5b5c6be8a71
Saranjith <saranvlmna@gmail.com>
2025-02-21 12:49:30 +0530

    add multer for file upload handling and update ocr extraction logic

--- src/ocr/ocr.extract.js
+++ src/ocr/ocr.extract.js
@@ -1,6 +1,7 @@
 module.exports = (req, res) => {
   try {
-    console.log(req.body);
+    console.log(req.file);
+    console.log(req.file.mimetype);
   } catch (error) {
     console.log(error);
   }
--- src/ocr/router.js
+++ src/ocr/router.js
@@ -1,5 +1,8 @@
-const ocrExtract = require("./ocr.extract");
 const ocrRouter = require("express").Router();
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() }); // No folder needed
+const ocrExtract = require("./ocr.extract");
 
-ocrRouter.post("/", ocrExtract)
+ocrRouter.post("/", upload.single("file"), ocrExtract);
+
 module.exports = ocrRouter;
