d823f14d40628776937857450e4234bffbd906c4
Saranjith <saranvlmna@gmail.com>
2025-02-26 17:47:06 +0530

    update: add Pinecone database dependency and introduce violation router

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import "dotenv/config";
 import express, { json } from "express";
-import ocrRouter from "./src/ocr/router.js";
 import cosmosConnection from "./database/cosmos.connection.js";
+import ocrRouter from "./src/ocr/router.js";
+import violationRouter from "./src/violation/router.js";
 
 const app = express();
 const port = process.env.APP_PORT || 4579;
-await cosmosConnection()
+await cosmosConnection();
 
 app.use(json());
+
 app.use("/ocr", ocrRouter);
+app.use("/violation", violationRouter);
 
 app.listen(port, () => {
   console.log(`ocr violation management server listening at ${port}`);
