3488b3c06bb529b95fe03af7002982ddf85227e5
Saranjith <saranvlmna@gmail.com>
2025-02-26 17:48:00 +0530

    update: add violation search endpoint using vector search functionality

---
+++ src/violation/violation.search.js
@@ -0,0 +1,14 @@
+import vectorSearch from "./lib/vector.search.js";
+
+export default async (req, res) => {
+  try {
+    const { key } = req.query;
+    
+    const result = await vectorSearch(key);
+
+    return res.json({ result });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
